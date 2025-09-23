79fcdd579d63b38080f6ffebe660461aae5e17c2
Josna Mathew <josnamathew875@gmail.com>
2025-09-23 23:24:05 +0530

    module wrapper

---
+++ iife.js
@@ -0,0 +1,9 @@
+(function  (message){
+    const superH = "tanjiro"
+    console.log(message,superH);
+})("Hello");
+
+(function  (message){
+    const superH = "Akaaza"
+    console.log(message,superH);
+})("Hey");
